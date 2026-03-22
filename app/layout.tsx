b4864aa9abe3167f8ad561ea51d06d13668a02fd
taozgt <2959131933@qq.com>
2026-03-22 15:52:32 +0800

    feat

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
@@ -62,6 +63,7 @@ export default function RootLayout({
           <Navbar />
           {children}
           <Toaster />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
